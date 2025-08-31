4aef702d1f5f254c9eb179e8bbc1150569c8abd8
TG <tiangooden@gmail.com>
2025-08-31 17:52:21 -0500

    update doc schema

--- src/app/api/files/schema.ts
+++ src/app/api/files/schema.ts
@@ -1,6 +1,9 @@
 import z from "zod";
 
 export const documentsSchema = z.object({
-    type: z.string().min(2, "Document type required"),
-    url: z.url("Invalid document URL"),
+  name: z.string().min(1, "Name is required."),
+  type: z.string().min(1, "Type is required."),
+  size: z.number().int("Size must be an integer.").positive("Size must be a positive number."),
+  key: z.string().optional(),
+  verified: z.boolean().optional(),
 });
